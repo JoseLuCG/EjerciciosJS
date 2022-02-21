7265576bcec0d4e2a1b85a07378fb57a67e79790
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:45:41 +0100

    Solucion ejercicio 7

--- src/for and if/exercise7/index.js
+++ src/for and if/exercise7/index.js
@@ -2,3 +2,6 @@ const numbers = ["Frodo","Gandalf","Turin","Sauron","Saruman","Bilbo"];
 
 // Put your code here
 
+for (let counter = 4; counter <= 6; counter++){
+    console.log(numbers[counter]);
+}
