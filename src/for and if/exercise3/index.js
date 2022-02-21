e440bd13995f03c1ad407c8fee8619be8d301a2b
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:04:19 +0100

    Solucion ejercicio 3

--- src/for and if/exercise3/index.js
+++ src/for and if/exercise3/index.js
@@ -1,7 +1,11 @@
 const numbers = [0,99,3,121,12,1,2];
 let sum = 0;
 let average = 0;
+let counter = 0;
 
-// Put your code here
-
-console.log(average)
+for (let item of numbers) {
+    sum = sum + item;
+    counter++;
+}
+average = sum/counter;
+console.log("La media es:", average)
