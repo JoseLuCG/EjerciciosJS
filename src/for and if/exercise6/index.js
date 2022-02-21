b55af5cb53d73cd75aa08a4c63b2e7a6a7ba1ed8
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:28:37 +0100

    Solucion ejercicio 6

--- src/for and if/exercise6/index.js
+++ src/for and if/exercise6/index.js
@@ -1,8 +1,13 @@
 const mix = [undefined,22,"1",4,3,"5",7,NaN,8,"9","12",11,null];
+let strings = [];
+let numbers = [];
 
 for (let item of mix) {
-    let strings = [];
-    let numbers = [];
+    if (typeof item === "number"){
+        numbers.push(item);
+    } else {
+        strings.push(item);
+    }
     // Put your code here
 }
 
