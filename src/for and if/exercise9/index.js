3bc0e0b99735bbdef5a598b42774cdee37f1534e
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-23 12:38:29 +0100

    Solucion ejercicio 9

--- src/for and if/exercise9/index.js
+++ src/for and if/exercise9/index.js
@@ -1,7 +1,7 @@
-let numbers = [0,1,2,3,4,5,6,7,8,9]
+let numbers = [0,1,2,3,4,5,6,7,8,9,10]
 
 for ( let left of numbers) {
     for (let right of numbers) {
-        // Put your code here
+        console.log(`${left}x${right}=${left*right}`)
     }
 }
