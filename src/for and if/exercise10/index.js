395e2e3adc13acc98538503967e38936bc4c3769
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-23 13:44:15 +0100

    Buscando la solucion del ejercicio 10

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,3 +1,11 @@
-let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
+let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3];
+let temp;
+for ( let idx = 0; idx < numbers.length; idx++ ){
+    console.log(numbers[idx]);
+    if (idx>idx+1){
+        numbers[idx]= temp;
+        numbers[idx]=numbers[idx+1];
+        numbers[idx]=temp;
+    }
 
-// Put your code here
+}
