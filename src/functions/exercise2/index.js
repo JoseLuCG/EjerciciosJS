892fccf7eb4eb874cea26aedb898ee84ec2ed692
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 13:34:27 +0100

    Solución ejercicio 2 Funciones Comentado

--- src/functions/exercise2/index.js
+++ src/functions/exercise2/index.js
@@ -1,8 +1,7 @@
 let number = 0;
-
+//Se cambia el <plusoen(number)> y el return <number> para no general ambiguedad.
 function plusone(sumNumber) {
     number = number + 1;
-    console.log(number);
     return sumNumber;
 }
 
