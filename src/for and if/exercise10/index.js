a4a06514c0d69b98f8c979042014e10240ab45ab
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 09:44:56 +0100

    Solucion ejercicio 10 corregido

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -2,11 +2,12 @@ let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3];
 let temp;
 
 for (let idx = 0; idx < numbers.length; idx++){
-    for (let idx = 0; idx < numbers.length; idx++)
+    for (let idx = 0; idx < numbers.length; idx++){
         if (numbers[idx] > numbers[idx+1]){
         temp = numbers[idx];
         numbers[idx] = numbers[idx+1];
         numbers[idx+1] = temp;
         }
+    }
 }
 console.log(numbers);
