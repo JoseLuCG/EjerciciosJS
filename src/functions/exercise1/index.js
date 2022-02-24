026f867cda063e069c84c11350f924df2a71ba34
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 11:03:59 +0100

    Solucion Ejercicio 1 Funciones

--- src/functions/exercise1/index.js
+++ src/functions/exercise1/index.js
@@ -4,9 +4,14 @@ const numbers3 = [22,6,4,4,5,7,8,9,12,11];
 const numbers4 = [22,9,6,8,5,7,8,9,12,11];
 
 function minorValue(array) {
-
-    // Put your code here
-
+    let menor = Infinity;
+    
+    for (idx=0; idx < array.length; idx++){
+        if (array[idx] < menor){
+            menor = array[idx];
+        }
+    }
+    return menor
 }
 
 const minor1 = minorValue(numbers1);
