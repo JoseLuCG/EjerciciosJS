80286302c6042761811243b5f16df686ed456a01
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-24 09:22:23 +0100

    implementacion de cambio de posicion ejercicio 10

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,11 +1,11 @@
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3];
 let temp;
-for ( let idx = 0; idx < numbers.length; idx++ ){
-    console.log(numbers[idx]);
-    if (idx>idx+1){
-        numbers[idx]= temp;
-        numbers[idx]=numbers[idx+1];
-        numbers[idx]=temp;
-    }
 
+for ( let idx = 0; idx < numbers.length; idx++ ){
+    if (numbers[idx] > numbers[idx+1]){
+        temp = numbers[idx];
+        numbers[idx] = numbers[idx+1];
+        numbers[idx+1] = temp;
+    }
 }
+console.log(numbers);
