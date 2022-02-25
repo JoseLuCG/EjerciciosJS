0de470167041926af87a54c5b038e3412a993426
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-25 13:31:33 +0100

    Solución ejercicio 3 Funciones

--- src/functions/exercise3/index.js
+++ src/functions/exercise3/index.js
@@ -1,7 +1,11 @@
+//Se crea una funcion que genere una raiz, la raiz cuadradad por defecto.
 
+function root (rad, index=1/2) {
+    return rad**(index);
+}
 // Put your code here
 
-console.log(power(25))
-console.log(power(8,1/3))
-console.log(power(2,3))
-console.log(power(5,2))
+console.log(root(25))
+console.log(root(8,1/3))
+console.log(root(2,3))
+console.log(root(5,2))
