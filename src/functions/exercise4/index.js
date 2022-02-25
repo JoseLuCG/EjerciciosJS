4440e30c634fcdf6303e45bd146e793ba0e6c184
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-25 13:46:28 +0100

    Solucion ejercicio 4

--- src/functions/exercise4/index.js
+++ src/functions/exercise4/index.js
@@ -1,6 +1,11 @@
 const numbers = [11,12,13,14]
 
-// Put your code here
+function swap (number1,number2){
+    let temp;
+    temp=numbers[number1];
+    numbers[number1]=numbers[number2];
+    numbers[number2]=temp;
+}
 
 swap(0,3,numbers);
 console.log(numbers);
