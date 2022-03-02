a97c08be2d6eaa0bfbf3dd749e71a6a16d28caa5
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-02 10:20:16 +0100

    Solucion ejercicio 5 Funciones

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -1,9 +1,26 @@
-const numbers = [11,9,13,12]
+const numbers = [11, 9, 13, 12]
 
-function minorInRange (a,b, array) {
-    const minor = minorInRange(a,b, array)
+function minorInRange(a, b, array) {
+    let minor = Infinity;
+    for (idx = a; idx <= b; idx++) {
+        if (array[idx] < minor) {
+            minor = array[idx];
+        }
+    }
+    return minor
 }
 
-console.log(minorInRange(0,3,numbers));
-console.log(minorInRange(2,3,numbers));
-console.log(minorInRange(1,3),numbers);
+console.log(minorInRange(0, 3, numbers));
+console.log(minorInRange(2, 3, numbers));
+console.log(minorInRange(1, 3, numbers));
+
+/*
+     let menor = Infinity;
+    //escribir bien los condicionales en el orden correcto
+    for (idx=0; idx < array.length; idx++){
+        if (array[idx] < menor){
+            menor = array[idx];
+        }
+    }
+    return menor
+ */
