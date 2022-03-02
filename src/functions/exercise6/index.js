fb0691d0607c724e1c41f2274a7bc329b8db5878
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-02 13:45:04 +0100

    Buscando solucion ejercicio 6

--- src/functions/exercise6/index.js
+++ src/functions/exercise6/index.js
@@ -1,7 +1,18 @@
-const numbers = [11,9,13,12]
+const numbers = [11,9,13,12,0,99]
 
 // Paste your functions here
 
+
 // Put your code here
+for (idx = 0; idx < numbers.length; idx++) {
+    for(idx = 0; idx < numbers.length; idx++){
+        if (numbers[idx] > numbers[idx + 1]) {
+            let temp;
+            temp=numbers[idx];
+            numbers[idx]=numbers[idx+1];
+            numbers[idx+1]=temp;
+        }    
+    }
+}
 
 console.log(numbers);
