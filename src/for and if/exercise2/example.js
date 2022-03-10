34364f477d6947fe02a1acecb8ebb045c645fcda
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-10 13:21:59 +0100

    Solution of exercise 2 SR

--- src/for and if/exercise2/example.js
+++ src/for and if/exercise2/example.js
@@ -1,9 +1,9 @@
 const people = ["Mortadelo", "Filemon", "Ofelia", "Bacterio"];
 let counter = 0;
 console.log(counter);
-for (let idx of people) {
+for (let item of people) {
     //counter++;
-    console.log(people[counter]);
+    console.log(item);
     counter = counter+1;
 }
 console.log(counter);
