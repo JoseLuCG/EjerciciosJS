7d48c5161503e7628b42f8e290ec070e5d9bf18d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-10 13:38:29 +0100

    Solution exercise 3 SR

--- src/for and if/exercise3/index.js
+++ src/for and if/exercise3/index.js
@@ -1,7 +1,18 @@
 const numbers = [0,99,3,121,12,1,2];
 let sum = 0;
 let average = 0;
-
+let counter = 0;
 // Put your code here
+for (let item of numbers){
+    sum += item;
+    counter ++;
+}
+average = sum/counter;
+console.log("****************************");
+console.log("* Resultado de la suma:",sum,"*");
+console.log("****************************");
+console.log("Numero de elementos que dividen =>", counter);
 
-console.log(average)
+console.log("###################");
+console.log("# La media es:",average,"#");
+console.log("###################");
