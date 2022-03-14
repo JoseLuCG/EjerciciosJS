b4868b07330ba61b2d971c0b08d6aa4408044bef
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-14 11:12:53 +0100

    Solution exercise 6 SR

--- src/for and if/exercise6/index.js
+++ src/for and if/exercise6/index.js
@@ -1,9 +1,14 @@
 const mix = [undefined,22,"1",4,3,"5",7,NaN,8,"9","12",11,null];
-
+let strings = [];
+let numbers = [];
+let counter = 0;
 for (let item of mix) {
-    let strings = [];
-    let numbers = [];
     // Put your code here
+    if (typeof item === "string"){
+        strings.push(item);
+    }else {
+        numbers.push(item);
+    }
 }
 
 console.log("Strings:",strings,"Numbers:",numbers)
