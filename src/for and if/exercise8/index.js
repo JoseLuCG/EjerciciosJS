e720553e9b844cc4408c2e8f3bef3836c1599425
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-14 13:20:30 +0100

    Solution of exercise 8 SR

--- src/for and if/exercise8/index.js
+++ src/for and if/exercise8/index.js
@@ -1,1 +1,6 @@
 // Put your code here
+for (multiplying = 0; multiplying <= 10; multiplying++){
+    for (multiplier = 0; multiplier <= 10; multiplier++){
+        console.log(`${multiplying} x ${multiplier} = ${multiplier*multiplying}`);
+    }
+}
