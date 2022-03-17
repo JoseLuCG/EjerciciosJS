a4537e58d9acf06f1d52be46d83a9f509b4eed0d
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-17 10:46:35 +0100

    Solution of exercise 10 SR

--- src/for and if/exercise10/index.js
+++ src/for and if/exercise10/index.js
@@ -1,3 +1,15 @@
 let numbers = [2,1,4,5,6,3,7,8,1,4,6,8,1,5,7,8,3]
 
 // Put your code here
+
+for (let pos=0; pos < numbers.length; pos++){
+    for (let index = pos+1; index < numbers.length; index++){
+        if (numbers[pos]> numbers[index]){
+            let temp = null;
+            temp = numbers[pos];
+            numbers[pos] = numbers[index];
+            numbers[index] = temp;
+        }
+    }
+}
+console.log(numbers)
