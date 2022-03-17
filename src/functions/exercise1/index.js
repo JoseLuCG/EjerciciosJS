14c90549274bf32fa1c212b54033fcc100ca8c70
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-17 13:32:52 +0100

    Finding solution of functions exercise 1 SR

--- src/functions/exercise1/index.js
+++ src/functions/exercise1/index.js
@@ -4,13 +4,19 @@ const numbers3 = [22,6,4,4,5,7,8,9,12,11];
 const numbers4 = [22,9,6,8,5,7,8,9,12,11];
 
 function minorValue(array) {
-
     // Put your code here
-
+    let minorValue = Infinity;
+    for (let index = 0; index < array.length; index++){;
+        if (array[index] < minorValue){
+            minorValue = array[index]
+            //console.log(minorValue)
+        }
+    }
+    return minorValue;
 }
 
 const minor1 = minorValue(numbers1);
-console.log(minor1);
-console.log(minorValue(numbers2));
-console.log(minorValue(numbers3));
-console.log(minorValue(numbers4));
+console.log("Array 1: ", minor1);
+console.log("Array 2: ", minorValue(numbers2));
+console.log("Array 3: ", minorValue(numbers3));
+console.log("Array 4: ", minorValue(numbers4));
