cb2cd014c9580ea7e11a224ad2b14877b4dc0b3c
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-19 05:59:37 +0200

    Solution of exercise 4 Functions SR

--- src/functions/exercise4/index.js
+++ src/functions/exercise4/index.js
@@ -1,6 +1,12 @@
 const numbers = [11,12,13,14]
 
 // Put your code here
+function swap(x,y,array) {
+    let temp = null;
+    temp = array[x];
+    array[x] = array[y];
+    array[y] = temp;
+}
 
 swap(0,3,numbers);
 console.log(numbers);
