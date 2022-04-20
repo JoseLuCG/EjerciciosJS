d2edf081e7358ac930a72fcd31c350d1a03c00f1
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-20 05:56:54 +0200

    Solution of exercise 5 Functions SR

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -3,27 +3,14 @@ const numbers = [11,9,13,12]
 // Put your code here
 function minorInRange( x, y, array) {
     let minorValue = Infinity;
-    for (let idx = x, idx < y; idx++) {
+    for (let idx = x; idx <= y ; idx++) {
         if (array[idx] < minorValue){
             minorValue = array[idx];
         }
     }
+    return minorValue;
 }
 
 console.log(minorInRange(0,3,numbers));
 console.log(minorInRange(2,3,numbers));
-console.log(minorInRange(1,3),numbers);
-
-
-
-function minorValue(array) {
-    // Put your code here
-    let minorValue = Infinity;
-    for (let index = 0; index < array.length; index++){;
-        if (array[index] < minorValue){
-            minorValue = array[index]
-            //console.log(minorValue)
-        }
-    }
-    return minorValue;
-}
+console.log(minorInRange(1,3,numbers));
