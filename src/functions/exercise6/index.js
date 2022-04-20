57090e7318322bbc3225334d96a494ac60ea34a6
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-20 09:13:58 +0200

    Finding solution of exercise 6

--- src/functions/exercise6/index.js
+++ src/functions/exercise6/index.js
@@ -1,7 +1,27 @@
 const numbers = [11,9,13,12]
 
 // Paste your functions here
-
+function minorInRange( x, y, array) {
+    let minorValue = x;
+    for (let idx = x; idx <= y ; idx++) {
+        if (array[idx] < array[minorValue]){
+            minorValue = idx;
+        }
+    }
+    return minorValue;
+}
+function swap(x,y,array) {
+    let temp = null;
+    temp = array[x];
+    array[x] = array[y];
+    array[y] = temp;
+}
 // Put your code here
+
+function pepito(array){
+    let minor = minorInRange(0, 3, array);
+    swap(0, minor, array)
+}
+pepito(numbers)
 
 console.log(numbers);
